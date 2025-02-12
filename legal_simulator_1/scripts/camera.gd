3821555259b2cb51d9ea8cf952c0cd5050ae0b49
Finn <71945500+OmegaBaum@users.noreply.github.com>
2025-02-12 19:41:22 +0100

    Mach kamera

--- legal_simulator_1/scripts/camera.gd
+++ legal_simulator_1/scripts/camera.gd
@@ -2,8 +2,10 @@ extends Node3D
 
 var target
 const lerpSpeed = 5
-
+const rotation_lerp_speed = 10
 @onready var camera = $Camera3D
+
+@onready var smooth_target = $SmoothTarget
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,3 +16,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position = position.lerp(target.position, delta * lerpSpeed)
+	
+	smooth_target.position = lerp(smooth_target.position, target.position, delta * rotation_lerp_speed)
+	camera.look_at(smooth_target.position, Vector3.UP)
+	
