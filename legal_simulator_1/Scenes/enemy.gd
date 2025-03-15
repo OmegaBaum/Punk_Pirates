eaef240c944be41058e26128dc8a24de2a103a28
Finn <71945500+OmegaBaum@users.noreply.github.com>
2025-03-15 22:16:23 +0100

    implement basic enemy AI

--- legal_simulator_1/Scenes/enemy.gd
+++ legal_simulator_1/Scenes/enemy.gd
@@ -1,14 +1,15 @@
-extends Node3D
+extends CharacterBody3D
 
 @onready var target_player = $"../Player"
 @onready var health_bar: ProgressBar = $SubViewport/ProgressBar
-const SPEED = 5.0
+const SPEED = 4.0
 const JUMP_VELOCITY = 4.5
 var projectile = preload("res://objects/hurt_box.tscn")
 @onready var enemy_rotation = $EnemyRotation
 @onready var projectile_spawner = $EnemyRotation/ProjectileSpawner
 
 # AI
+@onready var nav = $NavigationAgent3D
 @export var range: float = 2
 @export var attack_cooldown: float = 2
 var current_cooldown_time: float = 0
@@ -26,9 +27,17 @@ func _physics_process(delta: float) -> void:
 		return
 	if position.distance_to(target_player.position) < range:
 		attack()
+	
+	var next_location = nav.get_next_path_position()
+	var current_location = global_transform.origin
+	var new_velocity = (next_location - current_location).normalized() * SPEED
+	
+	velocity = velocity.move_toward(new_velocity, 0.25)
+	move_and_slide()
+	nav.target_position = target_player.position
 	if current_cooldown_time > 0:
 		current_cooldown_time -= delta
-
+	
 func attack():
 	if current_cooldown_time <= 0:
 		var direction: Vector3 = (position - target_player.position).normalized()
