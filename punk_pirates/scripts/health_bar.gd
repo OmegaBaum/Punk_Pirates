e80b06abea745e9acbdcdc5e362945ebbdb4c8b0
Finn <71945500+OmegaBaum@users.noreply.github.com>
2025-03-16 14:50:15 +0100

    make health bars even cleaner

--- punk_pirates/scripts/health_bar.gd
+++ punk_pirates/scripts/health_bar.gd
@@ -3,7 +3,7 @@ extends Control
 var value = 0
 var max_value = 0
 
-const ANIM_SPEED = 10
+const ANIM_SPEED = 5
 
 @onready var progress_bar = $ProgressBar
 @onready var progress_bar_white = $ProgressBarWhite
@@ -16,7 +16,7 @@ const ANIM_SPEED = 10
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	progress_bar.value = value
-	progress_bar_white.value = lerpf(progress_bar_white.value, progress_bar.value, delta * ANIM_SPEED)
+	progress_bar_white.value = lerpf(progress_bar_white.value, progress_bar.value - 2, delta * ANIM_SPEED)
 
 func set_max_value(value):
 	max_value = value
